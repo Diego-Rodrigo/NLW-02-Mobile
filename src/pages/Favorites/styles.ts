46b5e4f7d867362a2f1f9037f7d6377ba1226902
Diego-Rodrigo <diego.rodrigo.santos@outlook.com.br>
2020-08-09 20:50:18 -0300

    first commit

---
+++ src/pages/Favorites/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+    container:{
+
+    },
+    favoriteList:{
+        marginTop: -40,
+        
+    } 
+})
+
+export default styles
---
+++ src/pages/Landing/styles.ts
@@ -0,0 +1,54 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor:'#8257e5',
+        justifyContent: 'center',
+        padding: 40
+    },
+    banner:{
+        width: '100%',
+        resizeMode:'contain'
+    },
+    title:{
+        fontFamily: 'Poppins_400Regular',
+        color:'#fff',
+        fontSize:20,
+        lineHeight: 30,
+        marginTop: 80
+    },
+    titleBold:{
+        fontFamily: 'Poppins_600SemiBold'
+    },
+    buttonsContainer:{
+        flexDirection: 'row',
+        marginTop: 40,
+        justifyContent: 'space-between'
+    },
+    button:{
+        height: 150,
+        width: '48%',
+        borderRadius: 8,
+        padding: 24,
+        justifyContent: 'space-between'
+    },
+    buttonPrimary:{
+        backgroundColor:'#9871f5'
+    },
+    buttonSecundary:{
+        backgroundColor:'#04d361'
+    },
+    buttonText:{
+        fontFamily: 'Archivo_700Bold',
+        color: '#FFF',
+        fontSize: 20
+    },
+    totalConnections:{
+        
+    }
+
+
+})
+
+export default styles
